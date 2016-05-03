7c6bbdb6967b6e1bb07dbc1a46b3947fca2222a9
Kane <eoinkane1@gmail.com>
2016-05-03 22:34:24 +0100

    added StaffRequired login for inventory

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,10 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 
-from .views import ProductDetailView, ProductListView
+from .views import ProductDetailView, ProductListView, VariationListView
 
 urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='products'),         #listing products in view
     url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
    # url(r'^(?P<id>\d+)', 'products.views.product_detial_view_func', name='product_detail_function'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,23 +1,76 @@
+from django.contrib import messages
+from django.db.models import Q
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView                   # listing products in view!
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 # Create your views here.
 
 
-from .models import Product
+from .models import Product, Variation
+from .mixins import StaffRequiredMixin
+from .forms import VariationInventoryFormSet
 
+class VariationListView(StaffRequiredMixin, ListView):  #listing products in view from ProductDetailView
+    model = Variation   #model class of Variation
+    queryset = Variation.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(VariationListView, self).get_context_data(*args, **kwargs)
+        context["formset"] = VariationInventoryFormSet(queryset=self.get_queryset())
+        return context
+
+    def get_queryset(self, *args, **kwargs):        #queryset for search bar function
+        product_pk = self.kwargs.get("pk")
+        if product_pk:
+            product = get_object_or_404(Product, pk=product_pk)
+            queryset = Variation.objects.filter(product=product)
+        return queryset
+
+    def post(self, request, *args, **kwargs):
+        formset = VariationInventoryFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save(commit=False)
+            for form in formset:
+                new_item = form.save(commit=False)
+                #if new_item.title:
+                product_pk = self.kwargs.get("pk")
+                product = get_object_or_404(Product, pk=product_pk)
+                new_item.product = product
+                new_item.save()
+
+            messages.success(request, "Your inventory and pricing has been updated.")
+            return redirect("products")
+        raise Http404
 
 class ProductListView(ListView):  #listing products in view from ProductDetailView
-    model = Product
+    model = Product      #model class of Product
     queryset = Product.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context["now"] = timezone.now()
+        context["query"] = self.request.GET.get("q")
         return context
+
+    def get_queryset(self, *args, **kwargs):        #queryset for search bar function
+        qs = super(ProductListView, self).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")   # GET.get means can be with or with out q in the url string and wont raise an error with out q in the url
+        if query:
+            qs = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+                )
+            try:
+                qs2 = self.model.objects.filter(
+                    Q(price=query)
+                )
+                qs = (qs | qs2).distinct() # .distinct ensures only one query is returned
+            except:
+                pass
+        return qs
 
 
 class ProductDetailView(DetailView):
