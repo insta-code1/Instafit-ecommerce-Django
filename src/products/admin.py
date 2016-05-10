6a8ee1d3e2407aa1d51d99227abdd0bfa031e49b
Kane <eoinkane1@gmail.com>
2016-05-10 23:15:30 +0100

    added product template

--- src/products/admin.py
+++ src/products/admin.py
@@ -6,9 +6,30 @@ from django.contrib import admin
 from .models import Product, Variation, ProductImage, Category, ProductFeatured
 
 
-admin.site.register(Product)
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+    extra = 0
 
-admin.site.register(Variation)
+
+class VariationInLine(admin.TabularInline):
+    model = Variation
+    extra = 0
+    max_num = 10
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'price']
+    inlines = [
+        ProductImageInLine,
+        VariationInLine
+    ]
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
+
+# admin.site.register(Variation)
 
 admin.site.register(ProductImage)
 
